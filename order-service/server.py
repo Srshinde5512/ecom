53b3733c8f4e6f80588d8022bb432bfcad2fba58
srshinde5512 <srshinde5512@gmail.com>
2025-12-02 20:38:00 +0530

    order

--- order-service/server.py
+++ order-service/server.py
@@ -3,6 +3,6 @@ app = Flask(__name__)
 
 @app.route("/", methods=['GET'])
 def root():
-    return "welcome to order service"
+    return "welcome to order service 2.0"
 
 app.run(host="0.0.0.0", port=4000)
